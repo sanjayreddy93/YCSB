a93c79476d512d1278d7ec8ecffd0810cdec4be1
sanjay reddy <sanjaybeerelli@gmail.com>
2018-10-24 14:55:11 +0200

    Update RedisClient.java

--- redis/src/main/java/com/yahoo/ycsb/db/RedisClient.java
+++ redis/src/main/java/com/yahoo/ycsb/db/RedisClient.java
@@ -18,6 +18,8 @@
 /**
  * Redis client binding for YCSB.
  *
+ * All YCSB records are mapped to a Redis *hash field*.  For scanning
+ * operations, all keys are saved (by an arbitrary hash) in a sorted set.
  */
 
 package com.yahoo.ycsb.db;
@@ -27,38 +29,63 @@ import com.yahoo.ycsb.DB;
 import com.yahoo.ycsb.DBException;
 import com.yahoo.ycsb.Status;
 import com.yahoo.ycsb.StringByteIterator;
-
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.Protocol;
-
+import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Properties;
 import java.util.Set;
-import java.util.Vector
+import java.util.Vector;
 
+import java.util.Map.Entry;
+import java.net.HttpURLConnection;
+import java.net.URLEncoder;
+import java.net.URL;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
+import redis.clients.jedis.Protocol;
+import java.util.Properties;
 
 /**
  * YCSB binding for <a href="http://redis.io/">Redis</a>.
  *
  * See {@code redis/README.md} for details.
  */
- public class RedisClient extends DB {
+public class RedisClient extends DB {
+
   private Jedis jedis;
 
   public static final String HOST_PROPERTY = "redis.host";
   public static final String PORT_PROPERTY = "redis.port";
   public static final String PASSWORD_PROPERTY = "redis.password";
 
-  public static final String INDEX_KEY = "_indices";
+  public static final String INDEX = "indices";
   public void init() throws DBException {
+    Properties props = getProperties();
+    int port;
+
+    String portString = props.getProperty(PORT_PROPERTY);
+    if (portString != null) {
+      port = Integer.parseInt(portString);
+    } else {
+      port = Protocol.DEFAULT_PORT;
+    }
+    String host = props.getProperty(HOST_PROPERTY);
+
+    jedis = new Jedis(host, port);
+    jedis.connect();
+
+    String password = props.getProperty(PASSWORD_PROPERTY);
+    if (password != null) {
+      jedis.auth(password);
+    }
   }
 
   public void cleanup() throws DBException {
   }
-  
+
   /*
    * Calculate a hash for a key to store it in an index. The actual return value
    * of this function is not interesting -- it primarily needs to be fast and
@@ -74,13 +101,12 @@ import java.util.Vector
   public Status read(String table, String key, Set<String> fields,
       Map<String, ByteIterator> result) {
     try {
-      String url = "https://o9doeirora.execute-api.eu-central-1.amazonaws.com/default/serverlessfunct/read/Log?SvId=primary&type=log&table=User&key="+key+"&";
-      if(fields!=null){
-        for (String field: fields){
-          url+="fields="+field+"&";
-        }
-      }
-      url = url.substring(0, url.length()-1);     
+      String url = "https://us-central1-serverless-functions-217415.cloudfunctions.net/function-1?table=user&key="+key;
+//      if(fields!=null){
+//        for (String field: fields){
+//          url+="fields="+field+"&";
+//       }
+//      url = url.substring(0, url.length()-1);     
       URL obj;
       HttpURLConnection con = null;
       obj = new URL(url);
@@ -89,21 +115,24 @@ import java.util.Vector
       con.setRequestProperty("Accept", "application/json");
       BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
       ObjectMapper mapper=new ObjectMapper();
-      String inputLine;
-      while ((inputLine = in.readLine()) != null) {
-        Map<String, String> object=new HashMap<String, String>();
-        object=mapper.readValue(inputLine, new TypeReference<Map<String, String>>(){});
-        for (Map.Entry<String, String> entry: object.entrySet()){
-          object.put(entry.getKey(), entry.getValue());
+      String inputLine; 
+      while ((inputLine = in.readLine()) != null) { 
+        Map<String, Map<String, String>> object=new HashMap<String, Map<String, String>>();
+        object=mapper.readValue(inputLine, new TypeReference<Map<String, Map<String, String>>>(){});
+        System.out.println(object.get(key));
+        Map<String, String> object2=new HashMap<String, String>();
+        object2=object.get("Message");
+        for (Map.Entry<String, String> entry: object2.entrySet()){
+          object2.put(entry.getKey(), entry.getValue());
         }
-        StringByteIterator.putAllAsByteIterators(result, object);
+        StringByteIterator.putAllAsByteIterators(result, object2);
       }
       in.close();
     } catch (Exception e) {
       e.printStackTrace();
       return Status.ERROR;
     }
-    return Status.OK;          
+    return Status.OK;
   }
 
   @Override
@@ -111,22 +140,30 @@ import java.util.Vector
       Map<String, ByteIterator> values) {
     try {
       Map<String, String> map = StringByteIterator.getStringMap(values);
-      String url = "https://o9doeirora.execute-api.eu-central-1.amazonaws.com/default/serverlessfunct/insert?table=User&key="+key+"&";
+      String url = "https://us-central1-serverless-functions-217415.cloudfunctions.net/function-1/insert";
+      String payload = "{\"table\":\"User\", \"key\":\""+key+"\", ";
       for (Map.Entry<String, String> field : map.entrySet()){
-        url+=field.getKey()+"="+URLEncoder.encode(field.getValue(), "UTF-8")+"&";
-      
+        payload+="\""+field.getKey()+"\":\""+URLEncoder.encode(field.getValue(), "UTF-8")+"\", ";
       }
-      url = url.substring(0, url.length()-1);     
+      payload = payload.substring(0, payload.length()-2);
+      payload+="}";
+      System.out.println(payload);
+      System.out.println(url);
       URL obj;
       HttpURLConnection con = null;
       obj = new URL(url);
       con = (HttpURLConnection) obj.openConnection();
-      con.setRequestMethod("GET");
+      con.setDoOutput(true);
+      con.setRequestProperty("Content-Type", "application/json");
       con.setRequestProperty("Accept", "application/json");
-      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-      in.readLine();
-      in.close();
-      jedis.zadd(INDEX_KEY, hash(key), key);
+      con.setRequestMethod("POST");
+      con.connect();
+      byte[] outputBytes = payload.getBytes("UTF-8");
+      OutputStream os = con.getOutputStream();
+      os.write(outputBytes);
+      os.close();
+      con.getResponseCode();
+      jedis.zadd(INDEX, hash(key), key);
     } catch (Exception e) {
       e.printStackTrace();
       return Status.ERROR;
@@ -138,7 +175,7 @@ import java.util.Vector
   @Override
   public Status delete(String table, String key) {
     try {
-      String url = "https://o9doeirora.execute-api.eu-central-1.amazonaws.com/default/serverlessfunct/delete?table=User&key="+key;
+      String url = "https://us-central1-serverless-functions-217415.cloudfunctions.net/function-1/delete?table=user&key="+key;
       URL obj;
       HttpURLConnection con = null;
       obj = new URL(url);
@@ -152,8 +189,9 @@ import java.util.Vector
       e.printStackTrace();
       return Status.ERROR;
     }
-    return jedis.zrem(INDEX_KEY, key) == 0 ?  Status.ERROR
+    return jedis.zrem(INDEX, key) == 0 ?  Status.ERROR
        : Status.OK;
+
   }
 
   @Override
@@ -161,46 +199,72 @@ import java.util.Vector
       Map<String, ByteIterator> values) {
     try {
       Map<String, String> map = StringByteIterator.getStringMap(values);
-      String url = "https://o9doeirora.execute-api.eu-central-1.amazonaws.com/default/serverlessfunct/update?table=User&key="+key+"&";
+      String url = "https://us-central1-serverless-functions-217415.cloudfunctions.net/function-1/update?table=user&key="+key+"&";
       for (Map.Entry<String, String> field : map.entrySet()){
         url+=field.getKey()+"="+URLEncoder.encode(field.getValue(), "UTF-8")+"&";
       }
-      url = url.substring(0, url.length()-1);     
+      url = url.substring(0, url.length()-1);
       URL obj;
       HttpURLConnection con = null;
       obj = new URL(url);
       con = (HttpURLConnection) obj.openConnection();
-      con.setRequestMethod("GET");
+      con.setDoOutput(true);
+      con.setRequestProperty("Content-Type", "application/json");
       con.setRequestProperty("Accept", "application/json");
-      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-      in.readLine();
-      in.close();
+con.setRequestMethod("POST");
+      con.connect();
+      con.getResponseCode();
     } catch (Exception e) {
       e.printStackTrace();
       return Status.ERROR;
     }
-    return Status.OK;          
+    return Status.OK;
   }
 
   @Override
   public Status scan(String table, String startkey, int recordcount,
       Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
     try {
-      String url = "https://o9doeirora.execute-api.eu-central-1.amazonaws.com/default/serverlessfunct/scanLog?table=User&key=";
-      url+=startkey+"&recordCount="+recordcount+"&";
-      Set<String> keys = jedis.zrangeByScore(INDEX_KEY, hash(startkey),
-      Double.POSITIVE_INFINITY, 0, recordcount);
-      HashMap<String, ByteIterator> values;
-      for (String key : keys) {
-      values = new HashMap<String, ByteIterator>();
-      read(table, key, fields, values);
-      result.add(values);
-   } 
+      String url = "https://us-central1-serverless-functions-217415.cloudfunctions.net/function-1/scan?table=user&key="+startkey;
+      url+="&recordCount="+recordcount;
+      if (fields!=null){
+        for (String field: fields){
+          url+="fields="+field+"&";
+        }
+      }
+      url = url.substring(0, url.length()-1);
+      URL obj = new URL(url);
+      HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+      con.setRequestMethod("GET");
+      con.setRequestProperty("Accept", "application/json");
+      BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+      String inputLine;
+      ObjectMapper mapper=new ObjectMapper();
+      while ((inputLine = in.readLine()) != null) {
+        Map<String, Map<String, String>> object=new HashMap<String, Map<String, String>>();
+        object=mapper.readValue(inputLine, new TypeReference<Map<String, Map<String, String>>>(){});
+        Map<String, String> object2=new HashMap<String, String>();
+        object2=object.get("Message");
+        for (Map.Entry<String, String> entry: object2.entrySet()){
+          object2.put(entry.getKey(), entry.getValue());
+        }
+        result.addElement((HashMap<String, ByteIterator>) StringByteIterator.getByteIteratorMap(object2));
+      }
+      in.close();
     }catch (Exception e) {
       e.printStackTrace();
       return Status.ERROR;
     }
     return Status.OK;
+  }
 
 }
+
+
+
+
+                                                                                                                                                                
+
+
+                                                                                                                                                                
 
